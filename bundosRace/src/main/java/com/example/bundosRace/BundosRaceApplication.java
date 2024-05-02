2a3d0d1cdced1f624112ec68183d63c7781e9340
jungryuel <zldrktduf0927@naver.com>
2024-05-02 18:20:19 +0900

    프로젝트 생성

---
+++ bundosRace/src/main/java/com/example/bundosRace/BundosRaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bundosRace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundosRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BundosRaceApplication.class, args);
+	}
+
+}
